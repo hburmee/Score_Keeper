e0ed6298daddd9226af4f8491d9a41a1dd704aa5
hburmee <37255507+hburmee@users.noreply.github.com>
2020-07-03 22:32:30 -0700

    input functional

--- score.js
+++ score.js
@@ -3,6 +3,8 @@ var p2button = document.querySelector("#p2");
 var reset = document.querySelector("#reset");
 var p1Display = document.querySelector("#p1display");
 var p2Display = document.querySelector("#p2display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0; 
 var gameOver = false;
@@ -34,3 +36,21 @@ p2button.addEventListener("click", function(){
 reset.addEventListener("click", function(){
 	location.reload();
 })
+
+function res(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = numInput.value;
+	winningScore = Number(numInput.value);
+	res();
+
+})
+
